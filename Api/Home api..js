ba204b8cb2ebc0297c7367474fe7973ac0394d27
vmanasa123 <111417202+vmanasa123@users.noreply.github.com>
2023-03-14 21:54:11 +0530

    Update Home api..js

--- Api/Home api..js
+++ Api/Home api..js
@@ -1,79 +1,163 @@
-const get = (req,res) =>{
-
+const get = (req, res) => {
   res.send([
-  
-    {
-      id: "101",
-      url:"https://upload.wikimedia.org/wikipedia/commons/8/85/TCS_SIPCOT_Building.jpg",
-      content:"Tata Consultancy Services (TCS) is an Indian multinational information technology (IT) services and consulting company with its headquarters in Mumbai.[6][7] It is a part of the Tata Group and operates in 150 locations across 46 countries.[8] In July 2022, it was reported that TCS had over 600,000 employees worldwide",
-      date:"On January 11, 2010.",
-      width: 540,
-      height: 427,
-      related:['Recent','Images','videos','news','Blogs'],
-    },
-    {
-      id: "102",
-      title:"Warner Bros. 2021 Film Slate - New Hybrid Distribution Model",
-      url:"https://bingeddata.s3.amazonaws.com/uploads/2020/12/warner-brothers-hbo-max-2021-movies-list.jpg",
-      content:"Warner Bros announced that it has committed to releasing its 2021 film slate via a unique, consumer-focused distribution model in which Warner Bros.",
-      category:"Wine",
-      date:"On December 07,2020",
-      width: 500,
-      height: 500,
-    },
-    {
-        id: "103",
-        url: " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgiFagSGE_G4KLwhATMOXwIzzO_LQ6mbqbVheqyzxeo474FwH-TXLsKfT6zog0BqJCM1U&usqp=CAU",
-        title:"Holi 2022: To play or not to play, opinions vary in Salt Lake & New Town",
-        content:"That is the dilemma people are facing on the day of the festival after two years of abstention but some are set to make the most of the occasion...",
-        category:"Celebration",
-        date:"On January 31, 2010",
-        width: 640,
-        height: 428,
+
+      {
+          title: "Dead Space Review: A Bloodcurdling Return to the Ishimura",
+          content: "Who would’ve guessed? 14 years later, the horror elements are still as effective",
+          publishedAt: "2 February 2023 19:44 IST",
+          author: "Rahul Chettiar",
+          urlToImage: "https://i.gadgets360cdn.com/large/Dead_Space_review_1675346226622.jpg?downsize=950:*",
+          url: "https://www.gadgets360.com/games/reviews/dead-space-review-remake-2023-gameplay-story-steam-pc-ps5-ea-motive-3747581#pfrom=home-reviews",
       },
-    {
-      id: "104",
-      title:'Konark Festival And International Sand Art Festival 2021 Kicks Off In Style',
-      url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd-OSxl8lMwuQfI_hAMGATnEc9KAoSNg-VOg&usqp=CAU",
-      content:"Konark Festival 2021, the five-day Indian classical dance extravaganza was inaugurated by Chief Minister Naveen Patnaik at Konark on Wednesday.",
-      category:"Celebration",
-      date:"On october 15,2016",
-      width: 500,
-      height: 351,
-    },
-    
-    {
-      id: "105",
-      title:"Lantern-Festival-Lanterns-Diagonal",
-      url:"https://tripgourmets.com/wp-content/uploads/2017/11/Hoi-An-Lantern-Festival-Lanterns-Diagonal.jpg",
-      content:" The emperor was overjoyed and ordered that on the 15th day of the first lunar month every year, people should make glutinous rice balls and hang up lanterns to worship the God of Fire. Because Yuanxiao was so good at it, people started to name the food after her, and from then on the day has been called the Lantern Festival or Yuan Xiao Jie.",
-      category:"Celebration",      
-      date:"On September 23, 2017.",
-      width: 500,
-      height: 307,
-    },
-    {
-        id: "106",
-        title:"Mountains and pink flowers",
-        url: "https://cdn.pixabay.com/photo/2015/04/23/22/01/mountains-736886__340.jpg",
-        content:"Free landscapes images to use in your next project. Browse amazing images uploaded by the Pixabay community.",
-        category:"Mountains",
-        date:"On 25 December 2012.",
-        width: 1193,
-        height: 1006,
+      {
+          title: "Embarrass You In 15 Years",
+          content: "Nick Jonas and Priyanka Chopra revealed the face of Malti Marie for the first time to the media at an event",
+          publishedAt: "ebruary 01, 2023 11:17 am IST",
+          author: " Pranita Chaubey",
+          urlToImage: "https://c.ndtvimg.com/2023-02/ctb0fh78_-nick-jonas-_625x300_01_February_23.jpg",
+          url: "https://www.ndtv.com/entertainment/nick-jonas-can-t-wait-to-embarrass-daughter-malti-marie-over-hollywood-walk-of-fame-star-after-15-years-3741728",
+
       },
-     {
-        id: "107",
-        title:'Boeing 737 Max Resumes Flying U.S. Passengers After 2-Year Halt.',
-       url:"https://i.insider.com/6047db54df53f8001802a56d?width=1136&format=jpeg",
-        content:"American Airlines used the plane, which was grounded in March 2019 after two fatal crashes, on a flight from Miami to New York.",
-        category:"Airlines",
-        date:"On october 15,2016",
-        width: 500,
-        height: 351,
+
+      {
+          title: "U.S. judge denies FTC request to stop Meta from acquiring VR firm Within - Reuters",
+          content: "imagesWASHINGTON, Feb 3 (Reuters) - A judge on Friday released a ruling denying the Federal Trade Commission's request to stop Meta Platforms Inc (META.O) from buying virtual reality content maker Within U… [+2079 chars]",
+          publishedAt: "2023-02-04T03:51:00Z",
+          author: "Husag samree",
+          urlToImage: "https://www.reuters.com/resizer/LNwvVgtTjo1bhc07WIb5Tfm3tc0=/960x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/CD3THFLR5VPRPB3JQYE4FWUV2I.jpg",
+          url: "https://www.reuters.com/legal/us-federal-judge-denies-us-ftc-request-stop-meta-acquiring-virtual-reality-2023-02-04/",
+
       },
-  
-    ])
-  }
-  
-  module.exports.apicontroller = get;
+      {
+          title: "The sushi has stopped revolving at Japan’s biggest revolving sushi restaurant chain",
+          content: "Sushiro is Japan’s largest and most popular kaitenzushi, or revolving sushi, restaurant chain. There are roughly 650 Sushiro locations across Japan, and if you’re heading to one anytime around regular dinner hours, usually you can pretty sure it’s going to be crowded with customers.",
+          publishedAt: "February 3-2023",
+          author: "Casey Baseel",
+          urlToImage: "https://soranews24.com/wp-content/uploads/sites/3/2023/02/SR-1-1.jpg",
+          url: "https://soranews24.com/2023/02/04/the-sushi-has-stopped-revolving-at-japans-biggest-revolving-sushi-restaurant-chain/",
+
+      },
+      {
+          title: "Hackers who breached ION say ransom paid; company declines comment",
+          content: "WASHINGTON, Feb 3 (Reuters) - The hackers who claimed responsibility for a disruptive breach at financial data firm ION say a ransom has been paid, although they declined to say ",
+          publishedAt: "February 4, 20238:38 AM GMT+5:30",
+          author: "By Raphael Satter",
+          urlToImage: "https://www.reuters.com/resizer/C9sFHbhZLLLHybKeYgHVVMiKJ0U=/960x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/OFOCCW3XVBIO7NURPXN2UGMF7Y.jpg",
+          url: "https://www.reuters.com/technology/hackers-say-ransom-paid-case-derivatives-data-firm-ion-company-declines-comment-2023-02-03/",
+
+      },
+      {
+          title: "Egg-mageddon: breakfast spots and bakeries suffer due to high egg prices",
+          content: "As egg prices have climbed steadily over the last year, consumers have the choice of forking over $14 for a carton of large brown eggs or attempting to go eggless to save some money at the grocery store.",
+          publishedAt: "20238:38 AM GMT+5:30",
+          author: "Frida Garza",
+          urlToImage: "https://ic-cdn.flipboard.com/guim.co.uk/11c5a9462c8cbb01627e6e053a5931f63668fb04/_medium.webp",
+          url: "https://flipboard.com/topic/newyorkcity/-/a-Ps19ezt5Q7WidhcuT8_ruQ%3Aa%3A3199676-%2F0",
+
+      },
+      {
+          title: "You Can Travel in a Vintage 1940s Train Along the Hudson River",
+          content: "If you thought the term “red-carpet treatment” came from Hollywood, think again. The phrase actually began with railroads in the early 1900s, when …",
+          publishedAt: "20238:38 AM GMT+5:30",
+          author: "Ella Feldman",
+          urlToImage: "https://th-thumbnailer.cdn-si-edu.com/5fUAdOxrahxLu-hzfs-ScPlfd9U=/fit-in/1600x0/filters:focal(1266x844:1267x845)/https%3A%2F%2Ftf-cmsv2-smithsonianmag-media.s3.amazonaws.com%2Ffiler_public%2F36%2F10%2F36108ed0-147c-4dd2-88da-c069f1267162%2Fpressphoto_2_cradamelmquist.png",
+          url: "https://flipboard.com/@johnjoseph9404/how-when-where-to-retire-travel-paepqrhqz/you-can-travel-in-a-vintage-1940s-train-along-the-hudson-river/a-CsM4G15dSzWrZuVZD4L0Ew%3Aa%3A74610229-2b1fc7a34a%2Fsmithsonianmag.com",
+
+      },
+      {
+          title: "Here’s Everything You Need To Know Before Buying Travel Insurance In 2023",
+          content: "Ask anyone with a recent travel insurance claim what they wish they'd known before buying their policy, and they'll probably answer without hesitation.",
+          publishedAt: "20238:38 AM GMT+5:30",
+          author: "Christopher Elliot",
+          urlToImage: "https://ic-cdn.flipboard.com/forbes.com/4d500a61c0e278d46c6aa595e3b731a2382b42be/_medium.webp",
+          url: "https://flipboard.com/@johnjoseph9404/how-when-where-to-retire-travel-paepqrhqz/here-s-everything-you-need-to-know-before-buying-travel-insurance-in-2023/a-3xHhu97aRB-mfSvrJmEWUg%3Aa%3A74610229-0a69e43d25%2Fforbes.com",
+
+      },
+      {
+          title: "London to Istanbul by train: This 10-day rail adventure is a slow traveller’s dream",
+          content: "Romantic cross-European train journeys have long been associated with the Orient Express. But you no longer need to fork out thousands to make this …",
+          publishedAt: "20238:38 AM GMT+5:30",
+          author: "Angela Symons",
+          urlToImage: "https://ic-cdn.flipboard.com/euronews.com/1a83ee9ebe129ff33b72a80eb6eda6e6e6d05c0f/_medium.webp",
+          url: "https://flipboard.com/@marvellousclare/wanderlust-fsvmdbj6z/london-to-istanbul-by-train-this-10-day-rail-adventure-is-a-slow-traveller-s-dr/a-83FZ-iovQcyI_2aSG_3u7Q%3Aa%3A70548169-fe60f82845%2Feuronews.com",
+
+      },
+
+      {
+          title: "Here’s how to make Homemade Cake with Idli batter ",
+          content: "Baking a cake is not an easy task. It takes a lot of hard work to perfect the art of cake making. Often, it is advised to bake a cake using the proper measurements. If this is not followed",
+          publishedAt: "-Feb 4, 2023, 08:08 IST",
+          author: "TIMESOFINDIA",
+          urlToImage: "https://static.toiimg.com/thumb/97593034.cms?width=680&height=512&imgsize=66644",
+          url: "https://timesofindia.indiatimes.com/life-style/food-news/heres-how-to-make-homemade-cake-with-idli-batter/photostory/97593024.cms",
+
+      },
+      {
+          title: "Do not go to bed feeling stressed and burned out; try this fitness drill to uplift your mood",
+          content: "Doing this 15 minutes workout will uplift your mood, release your stress and pave the path for a sound sleep, said Nidhi S, a yoga practitioner and fitness coach",
+          publishedAt: "January 30, 2023 19:30 IST",
+          author: "LifeStyle Desk",
+          urlToImage: "https://images.indianexpress.com/2022/07/skipping_200_getty.jpg",
+          url: "https://indianexpress.com/article/lifestyle/fitness/burnout-fitness-drill-mood-8046123/",
+
+      },
+      {
+          title: "Four reasons your lower back may be paining; and what to do about it",
+          content: "Fitness expert Namrata Purohit shares her tips and simple exercises on on how to fix lower back pain",
+          publishedAt: "Updated: January 30, 2023 12:11 IST",
+          author: "LifeStyle Desk",
+          urlToImage: "https://images.indianexpress.com/2023/01/Fix-lower-back-pain.jpg",
+          url: "https://indianexpress.com/article/lifestyle/fitness/lower-back-pain-and-what-to-do-about-it-8397939/",
+
+      },
+      {
+          title: "iPhone ‘Ultra’ in the Works, Said to Offer Better Features Than Pro Model",
+          content: "Samsung was the first smartphone manufacturer to launch a smartphone globally with the Ultra-branding. ",
+          publishedAt: "6 February 2023 12:27 IST",
+          author: "Sheldon Pinto",
+          urlToImage: "https://i.gadgets360cdn.com/large/iphone_14_pro_max_apple_1666942701797.jpg?downsize=950:*",
+          url: "https://www.gadgets360.com/mobiles/news/iphone-16-pro-max-ultra-model-expected-features-samsung-s23-ultra-mark-gurman-apple-3756740#pfrom=home-popular",
+      },
+      {
+          title: "Pathaan’s Comparison With James Bond, Mission Impossible",
+          content: "In a recent media interaction, Siddharth Anand talked about Pathaan getting compared with Hollywood hit franchises. Read on to know more!",
+          publishedAt: "February 2, 2023",
+          author: "koimoi-com-team",
+          urlToImage: "https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/02/pathaan-gets-compared-with-hollywood-movies-director-siddharth-anand-addresses-the-situation-001.jpg",
+          url: "https://www.koimoi.com/bollywood-news/pathaans-comparison-with-james-bond-mission-impossible-other-marvel-films-addressed-by-director-siddharth-anand-says-i-havent-watched-more-than-2-mcu-films/",
+
+      },
+      {
+          title: "Hera Pheri 3 Confirmed As The Director From ‘Bhool Bhulaiyaa 2’ Fame Anees Bazmee To",
+          content: "Reportedly, Dream Girl director Raaj Shaandilyaa has been approached to helm Hera Pheri",
+          publishedAt: "February 1, 2023",
+          author: "koimoi-com-team",
+          urlToImage: "https://static-koimoi.akamaized.net/wp-content/new-galleries/2023/02/hera-pheri-3-confirmed-as-the-director-from-bhool-bhulaiyaa-2-fame-anees-bazmee-to-dream-girl-maker-raaj-shaandilyaa-gets-changed-001.jpg",
+          url: "",
+
+      },
+
+      {
+          title: "Wells Fargo exec was raped by colleague, punished by bank, lawsuit says - New York Post",
+          content: "A senior vice president at Wells Fargo was raped by her colleague who barged into her hotel room and assaulted her while she was intoxicated during a business trip in Southern California, according t… [+8828 chars]",
+          publishedAt: "2023-02-03T22:34:00Z",
+          author: "Ariel Zilber",
+          urlToImage: "https://ic-cdn.flipboard.com/nypost.com/5faa7658164d2e1bc11fbf73a19e6461031529e1/_medium.webp",
+          url: "https://flipboard.com/topic/sexualharassment/-/a-jUwQNfJaRee2rWrk5Dh5Ig%3Aa%3A1944816596-%2F0",
+
+      },
+      {
+          title: "San Francisco sees 15X increase in “millionaire renters",
+          content: "Number of rich tenants ramped up nationwide over five years, RentCafe finds",
+          publishedAt: "2022-02-04T03:51:00Z",
+          author: "Emile Landes",
+          urlToImage: "https://therealdeal.com/sanfrancisco/wp-content/uploads/2023/02/SFO-San-Francisco-sees-15X-increase-in-millionaire-renters-MAIN.jpg",
+          url: "https://therealdeal.com/sanfrancisco/2023/02/01/san-francisco-sees-15x-increase-in-millionaire-renters/",
+
+      },
+
+  ])
+
+}
+
+module.exports.apicontroller = get;
